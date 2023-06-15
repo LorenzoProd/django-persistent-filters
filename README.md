ab6539851fd98720f29a04f214be67bcb5db7805
Lorenzo Prodon <lorenzo.prodon@gmail.com>
2023-06-15 15:24:20 +0200

    store filter in django request object

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Django Persistent Filters
 
 Django Persistent Filters is a Python package which provide a django middleware that take care to persist the
-querystring in the browser cookies.
+querystring in the browser cookies or in the Django Request object.
 
 If you have a ListView with a Form for filter the objects, this package is perfect for you!
 
@@ -24,6 +24,10 @@ MIDDLEWARE = [
 ]
 ```
 
+If you want to store filters in the Request object instead Cookies, add in the `settings.py` file:
+```python
+PERSISTENT_FILTERS_IN_REQUEST = True
+```
 Add the urls with a filter form in `settings.py` file:
 
 ```python
--- persistent_filters/middleware.py
+++ persistent_filters/middleware.py
@@ -7,28 +7,58 @@ class PersistentFiltersMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def _set_filters(self):
+        if self.use_request:
+            self.request.session[self.key] = self.query_string
+            return
+        self.response.set_cookie(
+            key=self.key,
+            value=self.query_string,
+            max_age=28800
+        )
+
+    def _delete_filters(self):
+        self.response = redirect(self.path)
+        if self.use_request:
+            if not self.request.session.get(self.key):
+                return
+            self.request.session.pop(self.key)
+            return
+        self.response.delete_cookie(self.key)
+
+    def _key_is_set(self):
+        if self.use_request:
+            return self.request.session.get(self.key)
+        return self.request.COOKIES.get(self.key)
+
+    def _get_redirect_url(self):
+        if self.use_request:
+            return self.request.path + '?' + self.request.session.get(self.key)
+        return self.request.path + '?' + self.request.COOKIES.get(self.key)
+
     def __call__(self, request):
-        response = self.get_response(request)
-        path = request.path_info
-        if path not in settings.PERSISTENT_FILTERS_URLS:
-            return response
+        self.use_request = False
+        if hasattr(settings, 'PERSISTENT_FILTERS_IN_REQUEST'):
+            self.use_request = getattr(settings, 'PERSISTENT_FILTERS_IN_REQUEST')
+        self.request = request
+        self.response = self.get_response(self.request)
+        self.path = request.path_info
+        self.key = 'filters{}'.format(self.path.replace('/', '_'))
 
-        query_string = request.META['QUERY_STRING']
+        if self.path not in settings.PERSISTENT_FILTERS_URLS:
+            return self.response
+
+        self.query_string = request.META['QUERY_STRING']
         if 'reset-filters' in request.META['QUERY_STRING']:
-            response = redirect(path)
-            response.delete_cookie('filters{}'.format(path.replace('/', '_')))
-            return response
+            self._delete_filters()
+            return self.response
 
-        if len(query_string) > 0:
-            response.set_cookie(
-                key='filters{}'.format(path.replace('/', '_')),
-                value=query_string,
-                max_age=28800
-            )
-            return response
+        if len(self.query_string) > 0:
+            self._set_filters()
+            return self.response
 
-        if len(query_string) == 0 and request.COOKIES.get('filters{}'.format(path.replace('/', '_'))):
-            redirect_to = request.path + '?' + request.COOKIES.get('filters{}'.format(path.replace('/', '_')))
+        if len(self.query_string) == 0 and self._key_is_set():
+            redirect_to = self._get_redirect_url()
             return HttpResponseRedirect(redirect_to)
 
-        return response
+        return self.response
