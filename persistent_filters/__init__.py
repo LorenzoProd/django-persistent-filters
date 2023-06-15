fa3204517d59dff7bd5a5b5f4b2d26f788f3c9c2
Lorenzo Prodon <lorenzo.prodon@gmail.com>
2023-06-15 15:35:04 +0200

    minor

--- persistent_filters/__init__.py
+++ persistent_filters/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Lorenzo Prodon'
-__version__ = '0.3'
+__version__ = '1.0'
 
 
 default_app_config = 'persistent_filters.apps.PersistenFiltersConfig'
